4c5ecd8a59ffb74e2f8075b83dc031ad2b334b10
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-07 14:36:21 +0530

    feat: completed the bascic grapql setup in /grapql route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import * as express from "express";
 import { config } from "dotenv";
+import * as express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema/schema";
 
 config();
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(`/graphql`, graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
 
 app.listen(port, () => console.log(`Server listening on port:${port}...`));
