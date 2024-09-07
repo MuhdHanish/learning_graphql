eb0258a006f1b6d2ba93bbd5dd7490a09dff1775
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-07 14:13:51 +0530

    feat: created a very simple server using express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log(`Hello from index.ts`);
+import * as express from "express";
+import { config } from "dotenv";
+
+config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server listening to port:${port}...`));
