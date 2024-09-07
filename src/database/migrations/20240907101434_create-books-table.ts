7ae82f6bca380292ae597d1d861c0daab24ac2ae
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-07 15:55:07 +0530

    feat: updated books table with actually coloumns

--- src/database/migrations/20240907101434_create-books-table.ts
+++ src/database/migrations/20240907101434_create-books-table.ts
@@ -2,6 +2,14 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("books", (table: Knex.TableBuilder) => {
+        table.uuid('id').primary().notNullable().unique();
+        table.string('title').notNullable();
+        table.integer('genre');
+        table.integer('rating');
+        table.uuid('author').references('id').inTable('authors');
+        table.timestamps(true, true);
+    })
 }
 
 
