cb62c260580f52a616cab8d9f0116ba1b68f7ff9
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-07 15:11:38 +0530

    chore: updated contance books with author

--- src/lib/contance.ts
+++ src/lib/contance.ts
@@ -1,14 +1,14 @@
 export const books = [
-    { id: '1', title: 'The Great Gatsby', genre: 'Fiction' },
-    { id: '2', title: '1984', genre: 'Dystopian' },
-    { id: '3', title: 'To Kill a Mockingbird', genre: 'Classic' },
-    { id: '4', title: 'The Catcher in the Rye', genre: 'Literary Fiction' },
-    { id: '5', title: 'Moby Dick', genre: 'Adventure' },
-    { id: '6', title: 'Pride and Prejudice', genre: 'Romance' },
-    { id: '7', title: 'War and Peace', genre: 'Historical Fiction' },
-    { id: '8', title: 'The Hobbit', genre: 'Fantasy' },
-    { id: '9', title: 'Crime and Punishment', genre: 'Psychological Fiction' },
-    { id: '10', title: 'The Odyssey', genre: 'Epic Poetry' }
+    { id: '1', title: 'The Great Gatsby', genre: 'Fiction', author: "5"},
+    { id: '2', title: '1984', genre: 'Dystopian', author: "3"},
+    { id: '3', title: 'To Kill a Mockingbird', genre: 'Classic', author: "2"},
+    { id: '4', title: 'The Catcher in the Rye', genre: 'Literary Fiction', author: "8"},
+    { id: '5', title: 'Moby Dick', genre: 'Adventure', author: "10"},
+    { id: '6', title: 'Pride and Prejudice', genre: 'Romance', author: "6"},
+    { id: '7', title: 'War and Peace', genre: 'Historical Fiction', author: "4"},
+    { id: '8', title: 'The Hobbit', genre: 'Fantasy', author: "1"},
+    { id: '9', title: 'Crime and Punishment', genre: 'Psychological Fiction', author: "9"},
+    { id: '10', title: 'The Odyssey', genre: 'Epic Poetry', author: "7" }
 ];
 
 export const authors = [
