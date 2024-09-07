73312020e4ed7ed40b93bd7d305de247bda77d64
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-07 14:43:48 +0530

    chore: added constace file and exported books

---
+++ src/lib/contance.ts
@@ -0,0 +1,12 @@
+export const books = [
+    { id: '1', title: 'The Great Gatsby', genre: 'Fiction' },
+    { id: '2', title: '1984', genre: 'Dystopian' },
+    { id: '3', title: 'To Kill a Mockingbird', genre: 'Classic' },
+    { id: '4', title: 'The Catcher in the Rye', genre: 'Literary Fiction' },
+    { id: '5', title: 'Moby Dick', genre: 'Adventure' },
+    { id: '6', title: 'Pride and Prejudice', genre: 'Romance' },
+    { id: '7', title: 'War and Peace', genre: 'Historical Fiction' },
+    { id: '8', title: 'The Hobbit', genre: 'Fantasy' },
+    { id: '9', title: 'Crime and Punishment', genre: 'Psychological Fiction' },
+    { id: '10', title: 'The Odyssey', genre: 'Epic Poetry' }
+];
